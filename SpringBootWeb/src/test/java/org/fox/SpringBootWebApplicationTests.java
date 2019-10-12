c493d660970084f932c3008af1ad3fcdd3f69e46
fox <18756001161@139.com>
2019-10-12 11:02:22 +0800

    初次

---
+++ SpringBootWeb/src/test/java/org/fox/SpringBootWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.fox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
